a732cab3686a310a1866beaf977677e03e72a5b7
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-05-19 21:34:53 +0900

    rubyバージョンアップ（2.3.1->2.5.1）による変更 ・bundle install ・deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.2"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:Hermiltonian/chat-space.git"
+set :repo_url, "git@github.com:Hermiltonian/twitter.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -42,7 +42,7 @@ set :linked_files, %w{ config/secrets.yml }
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'], keys: ['/Users/hiroshi/.ssh/hu-expert-1.pem']
 
