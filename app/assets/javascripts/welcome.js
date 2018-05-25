d1f274d1f7489cda5c48f1236707ea01481590ef
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-05-25 18:59:17 +0900

    ウェルカムページ用jsの修正 ・コメント追記 ・コンテンツの高さに下限値を設定追加

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,18 +1,35 @@
-function resizeContent() {
-  var bannerHeight = $("#banners").outerHeight(true);
-  var footerHeight = $(".StreamsFooter").outerHeight(true);
-  var windowHeight = $(window).height();
-  var contentHeight = windowHeight - bannerHeight - footerHeight;
-  $(".StaticLoggedOutHomePage-content").height(contentHeight);
+// 最適なビューを提供するためにメインコンテンツブロックの高さを調整する関数
+function adjustSizeOfContents() {
+  var bannerHeight = $("#banners").outerHeight(true);                 // バナーの高さ
+  var footerHeight = $(".StreamsFooter").outerHeight(true);           // フッターの高さ
+  var windowHeight = $(window).height();                              // ウィンドウの高さ
+  var contentHeight = 0;                                              // 現在のコンテンツの高さ
+  var newContentHeight = windowHeight - bannerHeight - footerHeight;  // 新たに設定しようとするコンテンツの高さ
+
+  // 現在のコンテンツの必要最低限の高さを取得
+  if ($(".StaticLoggedOutHomePage-signupBlock").outerHeight(true) > $(".StaticLoggedOutHomePage-communicationContent").outerHeight(true)) {
+    contentHeight = $(".StaticLoggedOutHomePage-signupBlock").outerHeight(true);
+  } else {
+    contentHeight = $(".StaticLoggedOutHomePage-communicationContent").outerHeight(true);
+  }
+
+  // 現在のコンテンツの方が高いなら高さを変えない
+  $(".StaticLoggedOutHomePage-content").height((contentHeight > newContentHeight) ? contentHeight : newContentHeight);
 }
 
 $(function() {
-  resizeContent();
+  // ページロード時のメインコンテンツブロックリサイズ
+  adjustSizeOfContents();
 
+  // バナーを確認した際のイベント
   $("#banners").on("click", "button", function() {
+    // バナーを消す
     $("#banners").empty();
-    resizeContent();
+
+    // バナー非表示後のメインコンテンツブロックをリサイズ
+    adjustSizeOfContents();
   });
 
-  $(window).resize(resizeContent);
+  // ウィンドウリサイズ時のメインコンテンツブロックをリサイズ
+  $(window).resize(adjustSizeOfContents);
 });
