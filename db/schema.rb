c5955c795d1e9225dcce750bae97a7531e670e87
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-05-31 17:58:51 +0900

    email単独では必須ではないため、インデックスが重複する可能性もあるため、インデックスを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_27_082124) do
+ActiveRecord::Schema.define(version: 2018_05_31_084437) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2018_05_27_082124) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
