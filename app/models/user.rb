756c7d31a2a47f22d2b9578bc04c74991878e910
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-06-03 15:35:50 +0900

    user.nameの自動生成メソッド実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,23 @@ class User < ApplicationRecord
 
   has_one :profile
 
+  # サインアップ時のUser.nameの自動生成
+  def create_name(profile_name)
+    key_length = 8
+    char_list = []
+    0.upto(25) { |n| char_list << (65 + n).chr }  # 英小文字
+    0.upto(25) { |n| char_list << (97 + n).chr }  # 英大文字
+    0.upto(9) { |n| char_list << n.to_s }         # 数字
+
+    new_name = profile_name + "_"
+    0.upto(key_length) { new_name += char_list.sample }
+
+    self.name = new_name
+  end
+
   protected
 
+  # devise :validatableのメソッドオーバーライド
   def email_required?
     false
   end
