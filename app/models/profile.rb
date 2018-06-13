bd822347cea9c2b89282c231f1fefa51764cff05
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-06-13 19:45:28 +0900

    profileモデルのバリデーションの実装

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 50, allow_blank: true }
+  validates :biography, length: { maximum: 160 }
+  validates :theme_color, presence: true, format: { with: /\A[0-9a-f]{6}\z/, allow_blank: true , message: "16進数で入力してください。" }
 end
